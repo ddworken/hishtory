7cdf9c0ac4e6c0f1d9ac7761f899c3b288061a53
David Dworken <david@daviddworken.com>
2022-10-21 22:58:51 -0700

    Escape instead of q so that the TUI can have search terms containing q

--- client/lib/tui.go
+++ client/lib/tui.go
@@ -94,7 +94,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "q", "ctrl+c":
+		case "esc", "ctrl+c":
 			m.quitting = true
 			return m, tea.Quit
 		case "enter":
