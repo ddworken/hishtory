7f8772af3a3ba78d9464c1b7195bcba36e2c6e95
David Dworken <david@daviddworken.com>
2023-11-01 22:09:49 -0700

    Fix test that I forgot to update in 588e2f4405a615abd41f8c1d15953b8c4f08effa

--- client/client_test.go
+++ client/client_test.go
@@ -1784,8 +1784,6 @@ func testTui_delete(t *testing.T) {
 	assertNoLeakedConnections(t)
 }
 
-// TODO: Write a test for control-X to select and cd
-
 func testTui_search(t *testing.T, onlineStatus OnlineStatus) {
 	// Setup
 	defer testutils.BackupAndRestore(t)()
