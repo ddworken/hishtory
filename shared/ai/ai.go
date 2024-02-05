80454d385972362201155decc5224792e8b5d616
David Dworken <david@daviddworken.com>
2024-02-04 17:32:20 -0800

    Add more debugging info when OpenAI API returns zero responses for #167

--- shared/ai/ai.go
+++ shared/ai/ai.go
@@ -103,7 +103,7 @@ func GetAiSuggestionsViaOpenAiApi(query, shellName, osName string, numberComplet
 		return nil, OpenAiUsage{}, fmt.Errorf("failed to parse OpenAI API response=%#v: %w", bodyText, err)
 	}
 	if len(apiResp.Choices) == 0 {
-		return nil, OpenAiUsage{}, fmt.Errorf("OpenAI API returned zero choices, resp=%#v", apiResp)
+		return nil, OpenAiUsage{}, fmt.Errorf("OpenAI API returned zero choices, parsed resp=%#v, resp body=%#v", apiResp, bodyText)
 	}
 	ret := make([]string, 0)
 	for _, item := range apiResp.Choices {
